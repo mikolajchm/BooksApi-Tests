e3c7bf172e3b056c3cb91d689150fee13771a608
mikolajchm <chmielewski.5454@gmail.com>
2024-09-20 17:29:29 +0200

    First commit

---
+++ utils/test/cutText.test.js
@@ -0,0 +1,12 @@
+const cutText = require('../cutText.js');
+const expect = require('chai').expect;
+
+
+it('should return an error if "content" arg is not a string', () => {
+    if(typeof content !== 'string') return 'Error';
+    expect(cutText(undefined, 20)).to.equal('Error');
+    expect(cutText(12, 20)).to.equal('Error');
+    expect(cutText({}, 20)).to.equal('Error');
+    expect(cutText([], 20)).to.equal('Error');
+    expect(cutText(function() {}, 20)).to.equal('Error');
+});
